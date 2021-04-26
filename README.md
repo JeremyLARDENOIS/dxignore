393cd5de8ae3b1763bfad36ec451d9077dd89848
jerem <jeremy.lardenois@hotmail.fr>
2021-04-26 22:33:26 +0200

    Build: First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# dxignore
+
+This tool allow you to ignore syncfile of Dropbox Linux
+
+## Documentation
+
+<https://help.dropbox.com/files-folders/restore-delete/ignored-files>
+
+## Pre-requisite
+
+Please, install `attr` before
+
+## Usage
+
+### Ignore a file or a repertory
+
+```bash
+dxignore [file]
+```
+
+### Know if a file or a repertory is ignored
+
+```bash
+dxignore -g [file]
+```
+
+### Stop ignore a file or a repertory
+
+```bash
+dxignore -r [file]
+```
+
+## Reporting
+
+If you need it for MacOs, don't hesitate to let a issue.
+You can let an issue, contribute, or just give feedback. It can help a lot !
+
+Enjoy !
---
+++ dxignore.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+help () {
+  echo "dxignore : Tools for disable dropbox sync"
+  echo ""
+  echo "dxignore [file]		ignore a file"
+  echo "dxignore -r [file]	don't ignore a file"
+  echo "dxignore -g [file]	say if a file is ignored"
+  echo "dxignore -h		help"
+}
+
+set() {
+  attr -s com.dropbox.ignored -V 1 $file &> /dev/null && echo "$file will be ignored by Dropbox" || echo "Error with file $file"
+}
+
+get () {
+  attr -g com.dropbox.ignored $file &> /dev/null && echo "$file is ignored by Dropbox" || echo "$file isn't ignored by Dropbox"
+}
+
+remove () {
+  attr -r com.dropbox.ignored $file &> /dev/null && echo "$file won't be ignoré by Dropbox" || echo "Error with file $file"
+}
+
+if [ $# -eq 1 ] && [ "$1" != "-h" ]; then
+  action="set" # Set ignoring by default
+  file=$1
+else
+  while getopts "hr:g:" arg; do
+    case $arg in
+      h)
+        help 
+        exit 0
+        ;;
+      r)
+        file=$OPTARG
+        action="remove"
+        ;;
+      g)
+        file=$OPTARG
+        action="get"
+        ;;
+      *)
+        echo "Bad usage"
+        help
+        exit 1
+    esac
+  done
+fi
+
+if [ ! -f $file ] && [ ! -d $file ] ; then
+  echo "$file file doesn't exist"
+  exit 1
+fi
+
+$action
+
